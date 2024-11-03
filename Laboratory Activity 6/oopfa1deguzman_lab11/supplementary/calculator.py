bd18ce64226c9d3aff53e1ede18e81082fe9ebb0
Aero-Kent-De-Guzman <qakdeguzman@tip.edu.ph>
2024-11-03 12:42:39 +0800

    added laboratory 6

--- Laboratory Activity 6/oopfa1deguzman_lab11/supplementary/calculator.py
+++ Laboratory Activity 6/oopfa1deguzman_lab11/supplementary/calculator.py
@@ -2,13 +2,17 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
+import re # this module is so that the calculator can check if the expressions are valid...
+          # the eval() function can also work but is limited to basic arithmetics...
+import math # this module is to enable calculation with sine & cosine, see line 126 & 127
+import os
 
 class Calculator(QMainWindow):
     
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Notepad")
-        self.setWindowIcon(QIcon('pythonico.ico'))
+        self.setWindowTitle("Calculator")
+        self.setWindowIcon(QIcon('calcPhoto.ico'))
         self.loadmenu()
         
         self.loadwidget()
@@ -23,58 +27,41 @@ class Calculator(QMainWindow):
         editButton.setShortcut('ctrl+M')
         editButton.triggered.connect(self.cleartext)
         editMenu.addAction(editButton)
-        
-        fontButton = QAction('Font', self)
-        fontButton.setShortcut('ctrl+D')
-        fontButton.triggered.connect(self.showFontDialog)
-        editMenu.addAction(fontButton)
-        
-        saveButton = QAction('Save', self)
-        saveButton.setShortcut('ctrl+S')
-        saveButton.triggered.connect(self.saveFileDialog)
-        fileMenu.addAction(saveButton)
-        
-        openButton = QAction('Open', self)
-        openButton.setShortcut('ctrl+O')
-        openButton.triggered.connect(self.openFileNameDialog)
-        fileMenu.addAction(openButton)
+
+        hisClearButton = QAction('Reset History', self)
+        hisClearButton.setShortcut('ctrl+shift+H')
+        hisClearButton.triggered.connect(self.ClearCalcHistory)
+        editMenu.addAction(hisClearButton)
         
         exitButton = QAction('Exit', self)
         exitButton.setShortcut('ctrl+Q')
         exitButton.setStatusTip('Exit Application')
         exitButton.triggered.connect(self.close)
         fileMenu.addAction(exitButton)
-        
-    def showFontDialog(self):
-        font, ok = QFontDialog.getFont()
-        if ok:
-            self.notepad.text.setFont(font)
+
+        historyButton = QAction('History', self)
+        historyButton.setShortcut('ctrl+H')
+        historyButton.setStatusTip('Calculator History')
+        historyButton.triggered.connect(self.CalcHistory)
+        fileMenu.addAction(historyButton)
+    
+    def CalcHistory(self):
+        CalcHistory = 'CalculatorHistory.txt'
+        if os.path.exists(CalcHistory):
+            os.startfile(CalcHistory)
+        else:
+            QMessageBox.warning(self, "Error", "No history file found.")
+    
+    def ClearCalcHistory(self):
+        with open('CalculatorHistory.txt', 'w') as txt:
+            txt.write('')
             
-    def saveFileDialog(self):
-        options = QFileDialog.Options()
-        # options = QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self, "Save Notepad File", "",
-                                                  "Text Files (*.txt);;Python Files (*.py);;All files (*)", options = options)
-        if fileName:
-            with open(fileName, 'w') as file:
-                file.write(self.notepad.text.toPlainText())
-                
-    def openFileNameDialog(self):
-        options = QFileDialog.Options()
-        # options = QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Open Notepad File", "",
-                                                  "Text Files (*.txt);;Python Files (*.py);;All files (*)", options = options)
-        if fileName:
-            with open(fileName, 'r') as file:
-                data = file.read()
-                self.notepad.text.setText(data)
-                
     def cleartext(self):
-        self.notepad.text.clear()
+        self.Calculator.textLine.setText('')
         
     def loadwidget(self):
-        self.notepad = calculatorButtons()
-        self.setCentralWidget(self.notepad)
+        self.Calculator = calculatorButtons()
+        self.setCentralWidget(self.Calculator)
 
 class calculatorButtons(QWidget):
 
@@ -87,40 +74,61 @@ class calculatorButtons(QWidget):
         self.setLayout(grid)
         
         names = [
-                '7', '8', '9', '/',''
-                '4', '5', '6', '*',''
-                '1', '2', '3', '-',''
-                '0', '.', '=', '+',''
-                '', '', '', '', '']
+                '7', '8', '9', '/',
+                '4', '5', '6', '*',
+                '1', '2', '3', '-',
+                '0', '.', '=', '+',
+                'C', 'sin(', '(', ')',
+                '**','cos(','','']
         
         self.textLine = QLineEdit(self)
-        grid.addWidget(self.textLine, 0,1,1,5)
+        grid.addWidget(self.textLine, 0, 1, 1, 5)
         
-        
-        positions = [(i,j) for i in range(1,7) for j in range(1,6)]
-        for position, name in zip(positions,names):
+        positions = [(i, j) for i in range(1, 7) for j in range(1, 5)]
+        for position, name in zip(positions, names):
             if name == '':
                 continue
             button = QPushButton(name)
-            button.clicked.connect(self.on_click)
+            button.clicked.connect(lambda checked, btn=name: self.on_click(btn))
             grid.addWidget(button, *position)
-            
-        self.setGeometry(300,300,300,150)
-        self.setWindowTitle('Grid Layout')
+        
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Calculator')
         self.show()
         
     @pyqtSlot()
-    def on_click(self):
-        if grid.button('1'):
-            self.textLine.insert('1')
-        if grid.button('2'):
-            self.textLine.insert('2')
+    def on_click(self, btn_text):
+        if btn_text == '=':
+            btn_num = self.textLine.text()
+            with open('CalculatorHistory.txt', 'a') as txt:
+                txt.write(btn_num + '\n')
 
-        
-        
-        
-        
-        
-        
-        
-        
+            if re.match(r'^[\d\+\-\*\/\.\(\) sincos]+$', btn_num):
+                btn_num = btn_num.replace('sin(', 'math.sin(')
+                btn_num = btn_num.replace('cos(', 'math.cos(')
+                if self.is_valid_expression(btn_num):
+                    result = eval(btn_num)
+                    result = round(result, 5)
+                    self.textLine.setText(str(result))
+                    with open('CalculatorHistory.txt', 'a') as txt:
+                        txt.write('=' + str(result) + '\n')
+                else:
+                    self.textLine.setText("Syntax Error")
+            else:
+                self.textLine.setText("Syntax Error")
+
+        elif btn_text == 'C':
+            self.textLine.setText('')
+        else:
+            self.textLine.insert(btn_text)
+
+    def is_valid_expression(self, expression):
+        unmatched_parenthesis = 0
+        for char in expression:
+            if char == '(':
+                unmatched_parenthesis += 1
+            elif char == ')':
+                unmatched_parenthesis -= 1
+                if unmatched_parenthesis < 0:
+                    return False
+        return unmatched_parenthesis == 0
--- Laboratory Activity 6/oopfa1deguzman_lab11/supplementary/main.py
+++ Laboratory Activity 6/oopfa1deguzman_lab11/supplementary/main.py
@@ -1,5 +1,11 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+import re # this module is so that the calculator can check if the expressions are valid...
+          # the eval() function can also work but is limited to basic arithmetics...
+import math # this module is to enable calculation with sine & cosine, see line 126 & 127
+import os
 from calculator import *
 
 if __name__ == '__main__':
